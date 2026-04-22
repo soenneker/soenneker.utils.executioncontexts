4b4fa9159df11629dfb0350a23910cc599ce08b0
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:48 -0500

    TUnit conversion

--- test/Soenneker.Utils.ExecutionContexts.Tests/ExecutionContextUtilTests.cs
+++ test/Soenneker.Utils.ExecutionContexts.Tests/ExecutionContextUtilTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Utils.ExecutionContexts.Tests;
 
 public sealed class ExecutionContextUtilTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
